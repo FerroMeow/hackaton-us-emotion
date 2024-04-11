9db5ccc4995b4e97efa554000a98082ff24c30fa
Konrad Balcerzak <kobdi4@gmail.com>
2024-04-11 21:46:02 +0200

    Fix login errors

--- emotek/src/routes/(page)/login/+page.svelte
+++ emotek/src/routes/(page)/login/+page.svelte
@@ -115,7 +115,11 @@
 	<div class="container mx-auto">
 		<div class="my-8 h-[1px] bg-gray-900"></div>
 		<h2 class="text-2xl">Zarejestruj się</h2>
-		<form id="register" class="grid grid-cols-2 gap-4" method="post">
+		<form
+			id="register"
+			class="grid grid-cols-2 gap-4"
+			on:submit|preventDefault|stopPropagation={() => {}}
+		>
 			<Input id="mail" bind:value={user_email} required>Adres email</Input>
 			<Input id="mail" bind:value={user_password} required type="password">Hasło</Input>
 			<Input id="number" type="number" bind:value={currentUserModel.birthYear} required>
@@ -129,7 +133,11 @@
 		</form>
 		<div class="my-8 h-[1px] bg-gray-900"></div>
 		<h2 class="text-2xl">Zaloguj się</h2>
-		<form id="login" class="grid grid-cols-2 gap-4" method="post">
+		<form
+			id="login"
+			class="grid grid-cols-2 gap-4"
+			on:submit|preventDefault|stopPropagation={() => {}}
+		>
 			<Input id="mail" type="email" bind:value={login_mail} required>Adres email</Input>
 			<Input id="password" type="password" bind:value={login_pass} required>Hasło</Input>
 			<Button on:click={login}>Zaloguj się</Button>
