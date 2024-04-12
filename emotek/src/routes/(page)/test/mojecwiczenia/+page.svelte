17946515ddf5e066f26d4a4dda8ea7a1543281f1
Konrad Balcerzak <kobdi4@gmail.com>
2024-04-12 07:46:15 +0200

    Make myexercises look nice

--- emotek/src/routes/(page)/test/mojecwiczenia/+page.svelte
+++ emotek/src/routes/(page)/test/mojecwiczenia/+page.svelte
@@ -4,6 +4,7 @@
 	import type { TrainingSession } from '$lib/types/collections/TrainingSession';
 	import type { TrainingSessionResult } from '$lib/types/collections/TrainingSessionResult';
 	import { startTest, nextImage, endTest } from '$lib/testing';
+	import Button from '$lib/Button.svelte';
 
 	export let data: layoutData;
 	const { emotions, db, auth } = data;
@@ -45,44 +46,67 @@
 	}
 </script>
 
-{#if !testStarted}
-	<h2>Własny test rozpoznawania emocji</h2>
-	<form method="POST" on:submit|preventDefault|stopPropagation={() => {}}>
-		<div>
-			<label for="emotions">Wybierz emocje:</label>
-			<select id="emotions" name="emotions" bind:value={picCategories} multiple>
-				{#each emotions as emotion}
-					<option value={emotion.eng}>{emotion.pl}</option>
-				{/each}
-			</select>
-		</div>
-		<p>
-			<label for="picture_count">Wybierz ile zdjęć z każdej kategori chcesz wyświetlic</label><input
-				type="number"
-				bind:value={picCount}
-				min="0"
-				max="10"
-				id="picture_count"
-			/>
+<div>
+	<div class="container mx-auto">
+		<h2 class="pt-16 text-4xl">Ćwicz rozpoznawanie emocji</h2>
+		<p class="mt-4 text-xl">
+			To ćwiczenie pozwala Ci wybrać emocje które według ciebie sprawiają ci problem, i ćwiczenie
+			ich.
 		</p>
-
-		<button type="submit" on:click={beginTest}>Rozpocznij swój test</button>
-	</form>
-{:else}
-	<div>
-		<h2>Ćwicz rozpoznawanie emocji</h2>
-		<div>
-			<img id="image" src={image ? image.URL : ''} alt="Zdjęcie do zdiagnozowania emocji" />
-			<div id="emotionSelector">
-				<label for="emotion">Wybierz emocję: </label>
-				<select id="emotion" bind:value={selectedEmotion}>
-					{#each emotions as emotion}
-						<option value={emotion['eng']}>{emotion['pl']}</option>
-					{/each}
-				</select>
-				<button on:click={next}>Następne</button>
+		{#if !testStarted}
+			<form method="POST" on:submit|preventDefault|stopPropagation={() => {}}>
+				<div>
+					<p>
+						<label for="emotions">Wybierz emocje:</label>
+					</p>
+					<p>
+						<select id="emotions" name="emotions" bind:value={picCategories} multiple>
+							{#each emotions as emotion}
+								<option value={emotion.eng}>{emotion.pl}</option>
+							{/each}
+						</select>
+					</p>
+				</div>
+				<div class="mt-4">
+					<p>
+						<label for="picture_count">Wybierz ile zdjęć z każdej kategori chcesz wyświetlic</label>
+					</p>
+					<p>
+						<input type="number" bind:value={picCount} min="0" max="10" id="picture_count" />
+					</p>
+				</div>
+				<p>
+					<Button type="submit" class="mt-4" on:click={beginTest}>Rozpocznij swój test</Button>
+				</p>
+			</form>
+		{:else}
+			<p class="mt-4 text-xl">Zaznacz poprawną emocję, i gdy jesteś pewien kliknij "Następny"</p>
+			<div class="mt-16 grid grid-cols-2">
+				<div class="self-center justify-self-center">
+					<p class="text-2xl">Wybierz emocję:</p>
+					<p id="emotionSelector" class="mx-auto p-8">
+						{#each emotions as emotion}
+							<span class="mx-4 inline-block">
+								<label for="emotion-{emotion.eng}" class="text-xl">{emotion.pl}</label>
+								<input
+									id="emotion-{emotion.eng}"
+									type="checkbox"
+									value={emotion.eng}
+									bind:group={selectedEmotion}
+								/>
+							</span>
+						{/each}
+						<select id="emotion" multiple bind:value={selectedEmotion}> </select>
+					</p>
+					<p>
+						<Button on:click={next}>Następne</Button>
+					</p>
+				</div>
+				<div>
+					<img id="image" src={image ? image.URL : ''} alt="Zdjęcie do zdiagnozowania emocji" />
+					<div id="result" style="display:none;"></div>
+				</div>
 			</div>
-			<div id="result" style="display:none;"></div>
-		</div>
+		{/if}
 	</div>
-{/if}
+</div>
