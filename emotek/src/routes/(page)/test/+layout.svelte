63253a01381b2e0306a93a26a816512d682c4429
Konrad Balcerzak <kobdi4@gmail.com>
2024-04-12 07:03:36 +0200

    FIx test nav

--- emotek/src/routes/(page)/test/+layout.svelte
+++ emotek/src/routes/(page)/test/+layout.svelte
@@ -36,20 +36,18 @@
 </script>
 
 {#if authenticated}
-	<div class="grid min-h-full grid-cols-[auto_1fr]">
-		<nav class="bg-pistachio-700 divide-pomp_and_power-300 divide-y-2 divide-solid px-6 py-4">
-			<h3 class="text-lg">Panel Administracyjny</h3>
-			<ul class="divide-pomp_and_power-300 divide-y-2 divide-solid">
+	<div class="grid min-h-full grid-rows-[auto_1fr]">
+		<nav class="bg-glaucous-700 divide-pomp_and_power-300 px-6 py-4">
+			<h3 class="text-2xl">Testy</h3>
+			<ul class="divide-pink_lavender-300 flex gap-4">
 				{#each testLinks as sideLink}
 					<li>
-						<a href="/admin/{sideLink.link}" class="text-pomp_and_power-300 text-xl"
-							>{sideLink.name}</a
-						>
+						<a href={sideLink.link} class="text-pomp_and_power-300 text-xl">{sideLink.name}</a>
 					</li>
 				{/each}
 			</ul>
 		</nav>
-		<div class="p-12">
+		<div class="bg-pink_lavender-800 p-12">
 			<slot />
 		</div>
 	</div>
